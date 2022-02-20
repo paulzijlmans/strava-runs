7b01299b85ab33db47a60047284685b43413928c
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-20 13:20:37 +0100

    Add strava auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const AUTH_ENDPOINT = "https://www.strava.com/oauth/authorize";
+
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const redirectUri = "http://localhost:3000";
+  const authUrl = `${AUTH_ENDPOINT}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=activity:read_all`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      if()
+      <a href={authUrl}>Connect With Strava</a>
     </div>
   );
 }
